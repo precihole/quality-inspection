3c129b580fc74d90d972d52b8cbc08521c5f1076
rehanansari389 <rehan@preciholesports.com>
2022-10-20 17:35:28 +0530

    file upload link change

--- quality_inspection/quality_inspection/doctype/qa_inspection/qa_inspection.js
+++ quality_inspection/quality_inspection/doctype/qa_inspection/qa_inspection.js
@@ -50,9 +50,10 @@ frappe.ui.form.on('QA Inspection', {
     if(!cur_frm.doc.__islocal){
         frm.set_df_property("upload_file", "options", `
             <div style="border: 1px solid rgb(201, 0, 1); overflow: hidden; margin: 15px auto; max-width: 900px;">
-<iframe scrolling="no" src="https://script.google.com/macros/s/AKfycbzYTJSQSjZdp6Ls-7W79XL83Nhs7hBlXbmy1PUFWX6iX0GisaaL/exec?docType=`+cur_frm.doc.doctype+`&formId=`+ cur_frm.doc.name +`" style="border: 0px none;height: 450px; width: 900px;">
+<iframe scrolling="no" src="https://script.google.com/a/macros/precihole.com/s/AKfycbwc-QPzQw-Qc9bbWPpGrBd1aeiZ849TcU9ucTUCmLLlDJbUZam32YXcVkjL3AWhKp8NOg/exec?docType=`+cur_frm.doc.doctype+`&formId=`+ cur_frm.doc.name +`" style="border: 0px none;height: 450px; width: 900px;">
 </iframe>
 </div>
+
             `); 
         }
         else{
